aedfc65d30992023a7b59f1f296267e513382982
rejkid <rejkid@gmail.com>
2023-08-24 21:20:31 +1000

    First cut

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { SubNavComponent } from './subnav.component';
+import { LayoutComponent } from './layout.component';
+import { OverviewComponent } from './overview.component';
+import { MatTableModule } from '@angular/material/table';
+import { MaterialModule } from '../material/material.module';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ScheduleAllocatorComponent } from './accounts/schedule.allocator.component';
+
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        AdminRoutingModule,
+    ],
+    declarations: [
+        SubNavComponent,
+        LayoutComponent,
+        OverviewComponent,
+
+        //ImportComponent,
+    ]
+})
+export class AdminModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  dateTimeFormat: 'DD-MM-YYYY HH:mm',
+  dateFormat: 'DD-MM-YYYY',
+
+  //apiUrl: 'https://49.176.185.39:4000'
+  apiUrl: 'https://rosterbackend.azurewebsites.net',
+  //apiUrl: 'http://localhost:4000'
+  ///apiUrl: 'http://rejkid.hopto.org:4000'
+  //apiUrl: 'https://rejkid.hopto.org:4000'
+
+  baseUrl: 'https://rosterbackend.azurewebsites.net'
+};
